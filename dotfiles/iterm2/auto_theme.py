4a0d05d7a03087a555a8291e15936717cd016f3c
Thanh Phan <thanhph111@gmail.com>
2022-10-11 23:49:44 +0700

    Add a script to auto change theme based on system theme

---
+++ dotfiles/iterm2/auto_theme.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+# Reference: https://gist.github.com/jamesmacfie/2061023e5365e8b6bfbbc20792ac90f8
+
+import iterm2
+
+
+async def main(connection):
+    async with iterm2.VariableMonitor(
+        connection, iterm2.VariableScopes.APP, "effectiveTheme", None
+    ) as mon:
+        while True:
+            # Block until theme changes
+            theme = await mon.async_get()
+
+            # Themes have space-delimited attributes, one of which will be light or dark
+            parts = theme.split(" ")
+            if "dark" in parts:
+                preset = await iterm2.ColorPreset.async_get(
+                    connection, "Multiplex Dark"
+                )
+            else:
+                preset = await iterm2.ColorPreset.async_get(
+                    connection, "Multiplex Light"
+                )
+
+            # Update the list of all profiles and iterate over them.
+            profiles = await iterm2.PartialProfile.async_query(connection)
+            for partial in profiles:
+                # Fetch the full profile and then set the color preset in it.
+                profile = await partial.async_get_full_profile()
+                await profile.async_set_color_preset(preset)
+
+
+iterm2.run_forever(main)
