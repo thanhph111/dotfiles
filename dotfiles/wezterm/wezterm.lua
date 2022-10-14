f9afbe199a134d621d8e4a27ec70cc4b99554435
Thanh Phan <thanhph111@gmail.com>
2022-10-14 08:39:08 +0000

    Reduce nested and remove a one-time function

--- dotfiles/wezterm/wezterm.lua
+++ dotfiles/wezterm/wezterm.lua
@@ -4,14 +4,6 @@ local wezterm = require "wezterm"
 DEFAULT_DARK_THEME_NAME = "Multiplex Dark"
 DEFAULT_LIGHT_THEME_NAME = "Multiplex Light"
 
-local function scheme_for_appearance(appearance)
-    if appearance:find "Light" then
-        return DEFAULT_LIGHT_THEME_NAME
-    else
-        return DEFAULT_DARK_THEME_NAME
-    end
-end
-
 local config = {
     font = wezterm.font("FiraCode Nerd Font", { weight = 500 }),
     font_size = 10,
@@ -32,7 +24,9 @@ local config = {
     hide_tab_bar_if_only_one_tab = true,
     tab_bar_at_bottom = true,
 
-    color_scheme = scheme_for_appearance(wezterm.gui.get_appearance()),
+    color_scheme = wezterm.gui.get_appearance():find "Light" and
+        DEFAULT_LIGHT_THEME_NAME or
+        DEFAULT_DARK_THEME_NAME,
     enable_scroll_bar = false,
     default_cwd = string.format("%s/Desktop", wezterm.home_dir),
 }
@@ -40,23 +34,26 @@ local config = {
 if wezterm.target_triple:find "windows%-msvc" then
     config.font = wezterm.font("FiraCode NF", { weight = 500 })
     config.default_prog = { "powershell.exe", "-NoLogo" }
-else
-    wezterm.on(
-        "window-config-reloaded",
-        function(window, pane)
-            wezterm.run_child_process {
-                "bash",
-                "-c",
-                "for pid in $(pgrep vim); do kill -SIGUSR1 $pid; done"
-            }
-        end
-    )
 
-    if wezterm.target_triple:find "apple%-darwin" then
-        config.font_size = 13
-        config.initial_rows = 37
-        config.window_decorations = "RESIZE"
+    return config
+end
+
+-- Send SIGUSR1 to every Vim processes to trigger a check on system theme
+wezterm.on(
+    "window-config-reloaded",
+    function(window, pane)
+        wezterm.run_child_process {
+            "bash",
+            "-c",
+            "for pid in $(pgrep vim); do kill -SIGUSR1 $pid; done"
+        }
     end
+)
+
+if wezterm.target_triple:find "apple%-darwin" then
+    config.window_decorations = "RESIZE"
+    config.font_size = 13
+    config.initial_rows = 37
 end
 
 return config
