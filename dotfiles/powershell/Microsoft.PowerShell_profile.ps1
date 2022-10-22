27902e8921d2e2ca5f3394b0d81117411068de1f
Thanh Phan <thanhph111@gmail.com>
2022-10-22 17:19:50 +0700

    Move a module to the profile as a function

--- dotfiles/powershell/Microsoft.PowerShell_profile.ps1
+++ dotfiles/powershell/Microsoft.PowerShell_profile.ps1
@@ -85,6 +85,17 @@ function Write-AllPoshThemes {
     }
 }
 
+function Get-ChildItemBySize {
+    Get-ChildItem @args |
+        Add-Member -Force -PassThru -Type ScriptProperty -Name Length -Value {
+            Get-ChildItem $this -Recurse -Force | Measure-Object -Sum Length | Select-Object -Expand Sum
+        } |
+        Sort-Object Length -Descending |
+        Format-Table @{ label = 'TotalSize (KB)'; expression = { [math]::Truncate($_.Length / 1KB) }; width = 14 },
+        @{ label = 'Mode'; expression = { $_.Mode }; width = 8 },
+        Name
+}
+
 function Measure-BootTimeShell {
     param(
         [Parameter(Mandatory = $false, ValueFromPipeline = $true)]
