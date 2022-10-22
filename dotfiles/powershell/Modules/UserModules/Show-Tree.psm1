545bd27264d008c4915b71212054218350c57152
Thanh Phan <thanhph111@gmail.com>
2022-10-22 17:28:30 +0700

    Reformat `Show-Tree` module

--- dotfiles/powershell/Modules/UserModules/Show-Tree.psm1
+++ dotfiles/powershell/Modules/UserModules/Show-Tree.psm1
@@ -6,14 +6,14 @@
  Script to show the layout of PowerShell namespaces (Trees) using ASCII
 
 .EXAMPLE
-PS> show-tree C:\ -MaxDepth 2
+PS> Show-Tree C:\ -MaxDepth 2
 
 .EXAMPLE
-PS> show-tree C:\ -MaxDepth 2 -NotLike Wind*,P* -Verbose
+PS> Show-Tree C:\ -MaxDepth 2 -NotLike Wind*,P* -Verbose
 
 .EXAMPLE
 PS> New-PSDrive -PSProvider Registry -Root HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\ -Name WINDOWS
-PS> Show-Tree Windows: -maxdepth 1 -ShowDirectory
+PS> Show-Tree Windows: -MaxDepth 1 -ShowDirectory
 
 .NOTE
 -Like and -NotLike feature only affect with files.
@@ -21,24 +21,24 @@ Otherwise, I don't know how to deal with this situation: parent folders are not 
 #>
 
 
-function format-type {
+function Format-Type {
     param(
         [Parameter(Mandatory = $true)]
         [System.IO.FileInfo]
-        $object
+        $Object
     )
-    foreach ($filesGroup in $extensionTypes.values) {
-        if ($filesGroup[1].contains([IO.Path]::GetExtension($object.Extension))) {
-            return $color = $filesGroup[0]
+    foreach ($filesGroup in $ExtensionColors.values) {
+        if ($filesGroup[1].contains([IO.Path]::GetExtension($Object.Extension))) {
+            return $filesGroup[0]
         }
     }
-    return $color = $null
+    return $null
 }
 
 
 function Show-Tree {
     [CmdletBinding()]
-    [Alias("st")]
+    [Alias('st')]
     [OutputType([string[]])]
     param(
         [Parameter(Position = 0)]
@@ -47,11 +47,11 @@ function Show-Tree {
         [Parameter()]
         [int]$MaxDepth = [int]::MaxValue,
 
-        #List of wildcard matches. If a directoryname matches one of these, it will be skipped.
+        #List of wildcard matches. If a directory name matches one of these, it will be skipped.
         [Parameter()]
         [String[]]$NotLike = $null,
 
-        #List of wildcard matches. If a directoryname matches one of these, it will be shown.
+        #List of wildcard matches. If a directory name matches one of these, it will be shown.
         [Parameter()]
         [String[]]$Like = $null
     )
@@ -66,18 +66,18 @@ function Show-Tree {
             [Parameter()]
             [int]$MaxDepth = [int]::MaxValue,
 
-            #List of wildcard matches. If a directoryname matches one of these, it will be skipped.
+            #List of wildcard matches. If a directory name matches one of these, it will be skipped.
             [Parameter()]
             [String[]]$NotLike = $null,
 
-            #List of wildcard matches. If a directoryname matches one of these, it will be shown.
+            #List of wildcard matches. If a directory name matches one of these, it will be shown.
             [Parameter()]
             [String[]]$Like = $null,
 
             [Parameter()]
             [String[]]$_Start = $null,
 
-            #Internal parameter used in recursion for formating purposes
+            #Internal parameter used in recursion for formatting purposes
             [Parameter()]
             [int]$_Depth = 0
 
@@ -87,14 +87,14 @@ function Show-Tree {
             return
         }
 
-        $Children = Get-ChildItem $path -ErrorAction Ignore | sort
+        $Children = Get-ChildItem $path -ErrorAction Ignore | Sort-Object
         # $Children = Get-ChildItem $path -ErrorAction Ignore | Where-Object PSIsContainer -eq $true | sort
 
-        $FilteredChildren = @()
-        foreach ($d in $Children) {
+        $filteredChildren = @()
+        foreach ($child in $Children) {
             foreach ($pattern in $NotLike) {
-                if ($d.Name -like $pattern) {
-                    Write-Verbose "Pattern: $Pattern. Skipping: $($d.Name)"
+                if ($child.Name -like $pattern) {
+                    Write-Verbose "Pattern: $pattern. Skipping: $($child.Name)"
                     continue
                 }
             }
@@ -102,57 +102,53 @@ function Show-Tree {
 
             if (!$like) {
                 $ShowThisDirectory = $true
-            }
-            else {
+            } else {
                 foreach ($pattern in $Like) {
-                    if ($d.Name -like $pattern) {
-                        Write-Verbose "Pattern: $Pattern. Including: $($d.Name)"
+                    if ($child.Name -like $pattern) {
+                        Write-Verbose "Pattern: $pattern. Including: $($child.Name)"
                         $ShowThisDirectory = $true
                         break;
                     }
                 }
             }
-            if (($ShowThisDirectory) -or ($d.PSIsContainer)) {
-                $FilteredChildren += $d
+            if (($ShowThisDirectory) -or ($child.PSIsContainer)) {
+                $filteredChildren += $child
             }
         }
 
-        foreach ($d in $FilteredChildren) {
-            if ($d -eq $FilteredChildren[-1]) {
-                $Symbol = $EndSymbol
-                $Increment = $BlankSymbol
+        foreach ($child in $filteredChildren) {
+            if ($child -eq $filteredChildren[-1]) {
+                $symbol = $EndSymbol
+                $increment = $BlankSymbol
+            } else {
+                $symbol = $BranchSymbol
+                $increment = $GuideSymbol
             }
-            else {
-                $Symbol = $BranchSymbol
-                $Increment = $GuideSymbol
-            }
-            Write-Host ("{0}{1}$NameSymbol" -f $($_Start),$Symbol) -NoNewline
+            Write-Host ("{0}{1}$NameSymbol" -f $($_Start), $symbol) -NoNewline
 
-            if ($d.PSIsContainer) {
-                Write-Host ($d.Name) -BackgroundColor $FolderColor
-                Get-Tree -path:($d.FullName) -_Depth:($_Depth + 1) -MaxDepth:$MaxDepth -NotLike:$NotLike -Like:$Like -_Start:(($_Start + $Increment | Out-String) -replace "`r|`n","")
-            }
-            else {
-                $Color = format-type $d
-                if ($Color) {
-                    Write-Host ($d.Name) -ForegroundColor $Color
-                }
-                else {
-                    Write-Host ($d.Name)
+            if ($child.PSIsContainer) {
+                Write-Host ($child.Name) -BackgroundColor $FolderColor
+                Get-Tree -path:($child.FullName) -_Depth:($_Depth + 1) -MaxDepth:$MaxDepth -NotLike:$NotLike -Like:$Like -_Start:(($_Start + $increment | Out-String) -replace "`r|`n", '')
+            } else {
+                $color = Format-Type $child
+                if ($color) {
+                    Write-Host ($child.Name) -ForegroundColor $color
+                } else {
+                    Write-Host ($child.Name)
                 }
             }
         }
     }
-    "$((gi $Path).Name)"
-    Get-Tree @PSBOundParameters
+    "$((Get-Item $Path).Name)"
+    Get-Tree @PSBoundParameters
 }
 
 $Indent = 3
 $BranchSymbol = [char]::ConvertFromUtf32(0x251C)
 $EndSymbol = [char]::ConvertFromUtf32(0x2514)
 $NameSymbol = ([char]::ConvertFromUtf32(0x2500)) * $Indent
-$GuideSymbol = "$([char]::ConvertFromUtf32(0x2502))$(" " * $Indent)"
-$BlankSymbol = " " * ($Indent + 1)
+$GuideSymbol = "$([char]::ConvertFromUtf32(0x2502))$(' ' * $Indent)"
+$BlankSymbol = ' ' * ($Indent + 1)
 
 # $Indent = 5
 # $BranchSymbol = [char]::ConvertFromUtf32(0x255F )
@@ -162,15 +158,81 @@ $BlankSymbol = " " * ($Indent + 1)
 # $BlankSymbol = " " * ($Indent + 1)
 
 
-$extensionTypes = @{
-    picture = @([ConsoleColor]::Green,@(".png",".gif",".jpg",".jpeg"))
-    doc = @([ConsoleColor]::red,@(".docx",".pdf"))
-    media = @([ConsoleColor]::Magenta,@(".mp3",".mp4"))
-    Archive = @([ConsoleColor]::Yellow,@(".7z",".gz",".rar",".tar",".zip"))
-    dll = @([ConsoleColor]::Green,@(".dll",".pdb"))
-    text = @([ConsoleColor]::darkyellow,@(".csv",".lg","markdown",".rst",".txt",".md"))
-    config = @([ConsoleColor]::Yellow,@(".cfg",".config",".conf",".ini",".gitignore",".yml"))
-    executable = @([ConsoleColor]::Blue,@(".exe",".exe*",".bat",".cmd",".py",".pl",".ps1",".psm1",".vbs",".rb",".reg",".fsx",".js"))
+$FolderColor = [ConsoleColor]::DarkGray
+$ExtensionColors = @{
+    picture    = @(
+        [ConsoleColor]::Green, @(
+            '.png'
+            '.gif'
+            '.jpg'
+            '.jpeg'
+        )
+    )
+    doc        = @(
+        [ConsoleColor]::red, @(
+            '.docx'
+            '.pdf'
+        )
+    )
+    media      = @(
+        [ConsoleColor]::Magenta, @(
+            '.mp3'
+            '.mp4'
+        )
+    )
+    Archive    = @(
+        [ConsoleColor]::Yellow, @(
+            '.7z'
+            '.gz'
+            '.rar'
+            '.tar'
+            '.zip'
+        )
+    )
+    dll        = @(
+        [ConsoleColor]::Green, @(
+            '.dll'
+            '.pdb'
+        )
+    )
+    text       = @(
+        [ConsoleColor]::DarkYellow, @(
+            '.csv'
+            '.lg'
+            'markdown'
+            '.rst'
+            '.txt'
+            '.md'
+        )
+    )
+    config     = @(
+        [ConsoleColor]::Yellow, @(
+            '.cfg'
+            '.config'
+            '.conf'
+            '.ini'
+            '.gitignore'
+            '.yml'
+        )
+    )
+    executable = @(
+        [ConsoleColor]::Blue, @(
+            '.bat'
+            '.cmd'
+            '.exe'
+            '.exe*'
+            '.fsx'
+            '.js'
+            '.pl'
+            '.ps1'
+            '.psm1'
+            '.py'
+            '.rb'
+            '.reg'
+            '.vbs'
+        )
+    )
 }
 
-$FolderColor = [ConsoleColor]::DarkGray
+
+Export-ModuleMember -Function Show-Tree -Alias st
