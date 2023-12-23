ee1fc431d48532db558994065f1ab3d40a3b3e15
Thanh Phan <thanhph111@gmail.com>
2023-12-23 23:06:33 +0700

    Add theme Multiplex for Blink

---
+++ home/.blink/Multiplex.js
@@ -0,0 +1,21 @@
+t.prefs_.set("color-palette-overrides", [
+    "#181a1b",
+    "#a91409",
+    "#38803a",
+    "#cc7a00",
+    "#0a6ab6",
+    "#522e92",
+    "#37aab9",
+    "#788187",
+    "#555b5e",
+    "#ff3078",
+    "#addd1e",
+    "#ffec16",
+    "#0287c3",
+    "#d10aff",
+    "#4ae3f7",
+    "#dcdfe4",
+]);
+t.prefs_.set("foreground-color", "#fffaf4");
+t.prefs_.set("background-color", "#0e1019");
+t.prefs_.set("cursor-color", "rgb(145, 128, 90)");
